e2fb5bbea3f125fd6d80fb348ac0c34f9df9d171
X-Ray2003 <134064048+X-Ray2003@users.noreply.github.com>
2023-10-20 22:05:25 +0500

    Add project files.

---
+++ Tyuiu.SafronovVV.Sprint3.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SafronovVV.Sprint3.Task0.V6.Lib;
+
+namespace Tyuiu.SafronovVV.Sprint3.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 1;
+            int stopValue = 15;
+            double res = ds.GetMultiplySeries(startValue, stopValue);
+            double wait = 0.001;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.SafronovVV.Sprint3.Task0.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SafronovVV.Sprint3.Task0.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SafronovVV.Sprint3.Task0.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("812bb8f8-6c03-4427-91d8-667d40c410ea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
